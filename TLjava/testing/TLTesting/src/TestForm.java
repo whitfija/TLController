62174b03e890af94b646343846aa1490c612e28d
3two <whitfija@gmail.com>
2020-01-12 04:49:28 -0500

    swing testing v1

--- TLjava/testing/TLTesting/src/TestForm.java
+++ TLjava/testing/TLTesting/src/TestForm.java
@@ -1,6 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TestForm {
     private JTextArea testingOmgTextArea;
     private JPanel panel1;
+    private JButton exitBtn;
+
+    public TestForm(JFrame aFrame) {
+        exitBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "Closing");
+                aFrame.dispose();
+            }
+        });
+    }
+    private static void makeFrameFullSize(JFrame aFrame) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        aFrame.setSize(screenSize.width, screenSize.height);
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new TestForm(frame).panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.pack();
+        //makeFrameFullSize(frame);
+        //frame.setState(Frame.NORMAL);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+    }
 }
