ac807fdf05ccd973e7e179e3ce16ae66b55e672a
3two <whitfija@gmail.com>
2020-01-15 21:57:03 -0500

    creating first jar, for test

--- TLjava/TL/TLController/src/Wifi.java
+++ TLjava/TL/TLController/src/Wifi.java
@@ -10,7 +10,7 @@ public class Wifi {
 
     public static void main(String[] args){
         JFrame frame = new JFrame("App");
-        frame.setContentPane(new Registration(frame).mainPanel);
+        frame.setContentPane(new Wifi(frame).mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(640, 480);
         frame.setUndecorated(true);
