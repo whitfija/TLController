25c04e3f57e21bdba444bfecf447330bb8705e55
3two <whitfija@gmail.com>
2020-01-15 21:51:17 -0500

    additional pages, portrait mode setup
    on raspberry

--- TLjava/TL/TLController/src/Home.java
+++ TLjava/TL/TLController/src/Home.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +12,7 @@ public class Home {
     private JButton btnRegister;
     private JButton btnForgot;
     private JLabel lblTitle;
+    private JButton btnWifi;
 
     public Home(JFrame frame) {
         //
@@ -23,13 +23,20 @@ public class Home {
                 Registration.main(null);
             }
         });
+        btnWifi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                frame.setVisible(false);
+                Wifi.main(null);
+            }
+        });
     }
 
     public static void main(String[] args){
         JFrame frame = new JFrame("App");
         frame.setContentPane(new Home(frame).mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(640, 480);
+        frame.setSize(480, 640);
         frame.setUndecorated(true);
         frame.setVisible(true);
 
--- TLjava/TL/TLController/src/Wifi.java
+++ TLjava/TL/TLController/src/Wifi.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class Wifi {
+
+    private JPanel mainPanel;
+
+    public Wifi(JFrame frame) {
+
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new Registration(frame).mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(640, 480);
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+
+    }
 }
